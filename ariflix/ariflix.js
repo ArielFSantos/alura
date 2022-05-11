22abf64b9e1061258ee62cd29912c048c11163ff
ArielFSantos <94645030+ArielFSantos@users.noreply.github.com>
2022-05-10 23:25:42 -0300

    1

--- ariflix/ariflix.js
+++ ariflix/ariflix.js
@@ -1,3 +1,18 @@
+function clicar(){
+
+    var filmes = document.getElementById("filme").value;
+    if(filmes.endsWith(".jpg")){
+        var listaFilme = "<img src=" + filmes + ">"
+    var exibir = document.getElementById("listaFilmes")
+    exibir.innerHTML = exibir.innerHTML + listaFilme;
+
+    }
+    else{
+    console.error("Enedeço invalido")
+    }
+    document.getElementById("filme").value = ""
+
+}  
 
 
-
+1
